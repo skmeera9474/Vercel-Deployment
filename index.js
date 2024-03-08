f9d74e38a592b6c0922bbcba4cfb4b1ad61ffebd
Nagul meera <nagulshaik2349@gmail.com>
2024-03-08 17:14:08 +0530

    vercel config

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 const port = 9000;
+app.use("/", (req, res) => {
+    res.json({ message: "Hello From Express App"});
+});
 
 app.listen(9000, () => {
     console.log(`Starting Server on Port ${port}`);
